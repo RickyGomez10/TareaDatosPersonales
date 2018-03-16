719d001c5b7d94961a5b9678ce3fe5a90a7600c5
Ricardo <Ricardo@LAPTOP-TT6V4F6F>
2018-03-16 17:21:45 -0600

    new commit

---
+++ src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Ricardo
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Nombre: Ricardo Gomez");
+        System.out.println("Apellidos: Gomez Quijano");
+        System.out.println("Edad: 19");
+        System.out.println("Fecha de nacimiento: 19 de Abril de 1998");
+        System.out.println("Hobby: Pasar en la U todo el dia :c");
+    }
+    
+}
